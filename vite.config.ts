18dda8ab81f384708b9bd2f7b7c6dd4d21e856ea
Baltaj Bhandal <bbhandal5@my.bcit.ca>
2026-01-30 12:08:45 -0800

    modified yaml file

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Baltaj-Bhandal-ts/",
   plugins: [react()],
-  base: '/Baltaj-Bhandal-ts/', 
 });
