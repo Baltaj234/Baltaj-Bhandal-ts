eec1abbbfd7dcd1ddcd80735e95e1eca92b5e9fa
Baltaj Bhandal <bbhandal5@my.bcit.ca>
2026-01-30 12:15:53 -0800

    Using react-router again

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
-import { HashRouter } from 'react-router-dom'
+import {router} from './routes/Routes'
+import { RouterProvider } from 'react-router'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+   <RouterProvider router={router} />
+  </StrictMode>,
 )
